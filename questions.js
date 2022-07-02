7d6e4d2807de3f7631bdacdf8c570278e3ebd1b1
Damilare Abogunrin <43679376+St-Thierry@users.noreply.github.com>
2022-07-02 07:19:49 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -54,10 +54,7 @@ let questions = [
       "eXTra Multi-Program Language",
       "eXamine Multiple Language"
     ]
-  },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
+  
   //   {
   //   numb: 6,
   //   question: "Your Question is Here",
